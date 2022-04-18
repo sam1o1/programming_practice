91fb3135443960824d74e093155843f3600c337c
Eslam Abdelghany <eslam322_1@hotmail.com>
2022-04-19 00:59:22 +0200

    refine the code with more formating and comments

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -4,10 +4,10 @@ def lengthOfLongestSubstring(s):
     input substring s numbers, spaces,letters
     output kongest substring
     """
-    #to store the substring
-    substring="" 
+    # to store the substring
+    substring = ""
     # the length of substring
-    longest=0
+    longest = 0
     # starts if the input length > 1
     if len(s) > 1:
         # to loop over the input substring
@@ -15,62 +15,47 @@ def lengthOfLongestSubstring(s):
             # adding chars only if the substring variable does have it
             if (char not in substring):
 
-                substring=substring+char  #concatenate chars to form substring
-                #check if the formed substring == len of input then returns the length
-                # ex: if user entered abc then the formed substring is abc 
+                substring = substring+char  # concatenate chars to form substring
+                # check if the formed substring == len of input then returns the length
+                # ex: if user entered abc then the formed substring is abc
                 if len(substring) == len(s) and len(substring) > longest:
                     return len(substring)
             # if the char in trun in substring
             else:
-                    if longest ==0 or len(substring) >= longest:
-                        longest = len(substring)
-                    """
+                if longest == 0 or len(substring) >= longest:
+                    longest = len(substring)
+                """
                     to find the position of the first char in the substring and removes it and anything before it
                     also removes the substring from the main substring (the input)
                     ex1 abcanf result >> bcanf 
                     ex2 lsasam result >> sam
                     """
-                    pos=substring.find(char)+1
-                    st=substring[:pos]
-                    s=s.replace(st,'',1)
-                    substring=substring.replace(substring[:pos],"",1)+char
-                    st=""
-                    pos=0        
+                pos = substring.find(char)+1
+                st = substring[:pos]
+                s = s.replace(st, '', 1)
+                substring = substring.replace(substring[:pos], "", 1)+char
+                st = ""
+                pos = 0
         return longest
-    # if the input length <= 1 to deal with empty and one-char input 
+    # if the input length <= 1 to deal with empty and one-char input
     else:
         return len(s)
 
-test_cases= { "": 0," " : 1,"c" :1,"aab":2,"dvdf":3,"tmmzuxt":5,
-"asljlj":4,"ohvhjdml":6,"loddktdji":5,"ggububgvfk":6,"pwwkew" :3,"abcabcbb":3,"bbbbb":1}
+
+test_cases = {"": 0, " ": 1, "c": 1, "aab": 2, "dvdf": 3, "tmmzuxt": 5,
+              "asljlj": 4, "ohvhjdml": 6, "loddktdji": 5, "ggububgvfk": 6, "pwwkew": 3, "abcabcbb": 3, "bbbbb": 1}
+
 
 def test():
-    for key,value in test_cases.items():
+    for key, value in test_cases.items():
         try:
-            assert lengthOfLongestSubstring(key)==value
-            output=lengthOfLongestSubstring(key)
-            print("case {} passed! expected {} output {}".format(key,value,output))
+            assert lengthOfLongestSubstring(key) == value
+            output = lengthOfLongestSubstring(key)
+            print("case {} passed! expected {} output {}".format(key, value, output))
         except:
-            output=lengthOfLongestSubstring(key)
-            print("case {} failed! expected {} but output {}".format(key,value,output))
+            output = lengthOfLongestSubstring(key)
+            print("case {} failed! expected {} but output {}".format(
+                key, value, output))
+
+
 test()
-            
-            
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-
-
-
-    
-
--- romanToInt.py
+++ romanToInt.py
@@ -1,21 +1,23 @@
 
 def romanToInt(text):
-    dictionary={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-    text=text.upper()
-    sum_=[]
-    i=0
-    if 1 <=len(text) <= 15:
+    dictionary = {"I": 1, "V": 5, "X": 10,
+                  "L": 50, "C": 100, "D": 500, "M": 1000}
+    text = text.upper()
+    sum_ = []
+    i = 0
+    if 1 <= len(text) <= 15:
         while(i+1 < len(text)):
             if dictionary[text[i]] >= dictionary[text[i+1]]:
                 sum_.append(dictionary[text[i]])
-                text=text.replace(text[i],'',1)
-            else :
+                text = text.replace(text[i], '', 1)
+            else:
                 sum_.append(dictionary[text[i+1]]-dictionary[text[i]])
-                text=text.replace(text[i:i+2],'',1)  
-        if len(text)==1:
+                text = text.replace(text[i:i+2], '', 1)
+        if len(text) == 1:
             sum_.append(dictionary[text])
     else:
         return "Invaild input"
     return sum(sum_)
+
 
 print(romanToInt("iv"))
